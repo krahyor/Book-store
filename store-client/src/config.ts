2f6c47ff2725f6b7066a29da9bb67e90819d3d57
นพเก้า ปัญจสุธารส <myosuka@gmail.com>
2022-12-15 16:23:14 +0700

    ทำการปรับแต่งให้เรียกข้อมูลจาก Backend (จบ step1)

--- store-client/src/config.ts
+++ store-client/src/config.ts
@@ -2,7 +2,7 @@ const isDev = process.env.NODE_ENV || process.env.NODE_ENV === 'development'
 
 const config = {
   isDev,
-  remoteRepositoryUrlPrefix: isDev ? 'http://localhost:8000/api' : '/api'
+  remoteRepositoryUrlPrefix: isDev ? 'http://localhost:8000/api/category' : '/api'
 }
 
 export default config
--- store-client/src/models/Category.ts
+++ store-client/src/models/Category.ts
@@ -2,3 +2,4 @@ export default interface Category {
     id: number;
     title: string;
   }
+  
