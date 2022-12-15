dadd6e8a58a57e3ecfd3c7598ec7b7bd675cd161
นพเก้า ปัญจสุธารส <myosuka@gmail.com>
2022-12-15 22:07:26 +0700

    ปรับแต่งเพื่อเรียกใช้งานจาก Backend(จบ step2)

--- store-client/src/config.ts
+++ store-client/src/config.ts
@@ -2,7 +2,7 @@ const isDev = process.env.NODE_ENV || process.env.NODE_ENV === 'development'
 
 const config = {
   isDev,
-  remoteRepositoryUrlPrefix: isDev ? 'http://localhost:8000/api/category' : '/api'
+  remoteRepositoryUrlPrefix: isDev ? 'http://localhost:8000/api/book' : '/api'
 }
 
 export default config
