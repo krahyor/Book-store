cf25ff45566d19629d6d02e8f5e350c03cd3b4cf
นพเก้า ปัญจสุธารส <myosuka@gmail.com>
2022-12-15 22:06:08 +0700

    เพิ่ม Router ใน เพื่อใช้งาน Book

--- store-server/src/api/index.ts
+++ store-server/src/api/index.ts
@@ -1,8 +1,11 @@
 import Router from "koa-router"
 import category from './category'
+import book from './book'
 
 const apiRouter = new Router()
 
 apiRouter.use('/api/category',category.routes())
+apiRouter.use('/api/book', book.routes())
+
 
 export default apiRouter
