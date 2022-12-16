e0691d6148f8f59b224d3d39f3f42710238fe9a1
นพเก้า ปัญจสุธารส <myosuka@gmail.com>
2022-12-16 17:00:19 +0700

    แก้ไข config เพื่อเชื่อมกับ Backend

--- store-client/src/config.ts
+++ store-client/src/config.ts
@@ -2,7 +2,7 @@ const isDev = process.env.NODE_ENV || process.env.NODE_ENV === 'development'
 
 const config = {
   isDev,
-  remoteRepositoryUrlPrefix: isDev ? 'http://localhost:8000/api/book' : '/api'
+  remoteRepositoryUrlPrefix: isDev ? 'http://localhost:8000/api' : '/api'
 }
 
 export default config
