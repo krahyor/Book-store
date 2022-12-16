41ae9415c795f966650955b4b256abba2479d50e
นพเก้า ปัญจสุธารส <myosuka@gmail.com>
2022-12-16 16:59:17 +0700

    เติม callBackFn และส่วนทีเกี่ยวข้องกับการ submit

--- store-client/src/App.tsx
+++ store-client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import BookDetail from './components/BookDetail'
+import BookForm from './components/BookForm'
 import Book from './models/Book'
 import Category from './models/Category'
 import Repo from './repositories'
@@ -16,7 +17,39 @@ function App() {
     }
   }
 
-  return (   
+  const fetchCategoryList = async () => {
+    const result = await Repo.categories.getAll()
+    if (result) {
+      setCategoryList(result)
+    }
+  }
+
+  const onCreateBook = async (book: Partial<Book>) => {
+    await Repo.books.create(book)
+    fetchBookList()
+  }
+
+  const onUpdateBook = async (book: Partial<Book>) => {
+    await Repo.books.update(book)
+    fetchBookList()
+  }
+
+  const onDeleteBook = async (id: number) => {
+    await Repo.books.delete(id)
+    fetchBookList()
+  }
+
+  useEffect(() => {
+    fetchCategoryList()
+    fetchBookList()
+  }, [filter])
+
+  return (
+    <div>
+      <div>
+        <BookForm book={{}} categoryList={categoryList} callbackFn={onCreateBook} />
+        <hr />
+      </div>
       <div>
         <select onChange={e => setFilter(e.target.value)}>
           <option value={''}>All</option>
@@ -24,6 +57,15 @@ function App() {
         </select>
         <hr />
       </div>
+      {bookList.map(book =>
+        <div key={book.id}>
+          <BookDetail {...book} />
+          <BookForm book={book} categoryList={categoryList} callbackFn={onUpdateBook} />
+          <button onClick={e => onDeleteBook(book.id)}>Delete</button>
+          <hr />
+        </div>
+      )}
+    </div>
   );
 }
 
--- store-client/src/components/BookForm.tsx
+++ store-client/src/components/BookForm.tsx
@@ -5,6 +5,7 @@ import Category from "../models/Category";
 interface Props {
   book: Partial<Book>,
   categoryList: Category[],
+  callbackFn: (book: Partial<Book>) => void
 }
 
 function BookForm(props: Props) {
@@ -13,9 +14,22 @@ function BookForm(props: Props) {
   const stockAmountRef = useRef<HTMLInputElement>(null)
   const categoryRef = useRef<HTMLSelectElement>(null)
 
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    props.callbackFn({
+      id: props.book.id,
+      title: titleRef.current?.value,
+      price: Number(priceRef.current?.value),
+      stockAmount: Number(stockAmountRef.current?.value),
+      category: {
+        id: Number(categoryRef.current?.value)
+      }
+    })
+  }
 
   return (
     <div>
+      <form onSubmit={onSubmit}>
         <div>
           Title : <input type="text" defaultValue={props.book.title} ref={titleRef} required/>
         </div>
@@ -33,7 +47,9 @@ function BookForm(props: Props) {
           </select>
         </div>
         <button type="submit">Submit</button>
+      </form>
     </div>
+
   );
 }
 
