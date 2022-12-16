6497f378fb868f41d6bc45e365037f52abcaf8a8
นพเก้า ปัญจสุธารส <myosuka@gmail.com>
2022-12-16 11:38:58 +0700

    เลือกตัวแสดงหนังสือ All ในหน้าเว็บ(จบstep3)

--- store-client/src/App.tsx
+++ store-client/src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from 'react'
+import BookDetail from './components/BookDetail'
+import Book from './models/Book'
+import Category from './models/Category'
+import Repo from './repositories'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [bookList, setBookList] = useState<Book[]>([])
+  const [categoryList, setCategoryList] = useState<Category[]>([])
+  const [filter, setFilter] = useState<string>('')
+
+  const fetchBookList = async () => {
+    const result = await Repo.books.getAll({categoryId: filter})
+    if (result) {
+      setBookList(result)
+    }
+  }
+
+  return (   
+      <div>
+        <select onChange={e => setFilter(e.target.value)}>
+          <option value={''}>All</option>
+          {categoryList.map(category => <option key={category.id} value={category.id}>{category.title}</option>)}
+        </select>
+        <hr />
+      </div>
   );
 }
 
